1acd680cd73dffb9b9ed35fa568a879551ac3221
magic <samsgrice@gmail.com>
2022-01-21 12:22:39 +0100

    added excluded directories

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ if [ $# -eq 1 ]
 then
 	if [ $1 == "upload" ]
 	then
-		rsync -av --exclude="bundle/" --exclude=".*/" ~/.zsh* ~/.vim* ~/.color_files.py ~/Gits/Mine/dots/ 
+		rsync -av --exclude="cache/" --exclude="tmp/" --exclude="bundle/" --exclude=".*/" ~/.zsh* ~/.vim* ~/.color_files.py ~/Gits/Mine/dots/ 
 	fi
 else
-	rsync -av --exclude="bundle/" --exclude=".*/" ~/Gits/Mine/dots/.zsh* ~/Gits/Mine/dots/.vim* ~/Gits/Mine/dots/.color_files.py ~
+	rsync -av --exclude="cache/" --exclude="tmp/" --exclude="bundle/" --exclude=".*/" ~/Gits/Mine/dots/.zsh* ~/Gits/Mine/dots/.vim* ~/Gits/Mine/dots/.color_files.py ~
 fi
